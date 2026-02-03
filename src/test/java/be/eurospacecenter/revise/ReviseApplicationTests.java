d2335a810abe8652b9fc4a2066e2126a0d7cb5c9
Hugo Raskin <raskin.hugo@gmail.com>
2026-02-03 09:20:27 +0100

    init springboot

---
+++ src/test/java/be/eurospacecenter/revise/ReviseApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eurospacecenter.revise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
