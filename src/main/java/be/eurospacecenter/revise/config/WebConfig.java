9a1a53edd60d496879e6184f29537d854a114475
Hugo Raskin <raskin.hugo@gmail.com>
2026-02-13 17:47:32 +0100

    Add origin for test server

--- src/main/java/be/eurospacecenter/revise/config/WebConfig.java
+++ src/main/java/be/eurospacecenter/revise/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost", "https://revise-test.dev").allowCredentials(true);
+        registry.addMapping("/api/**").allowedOrigins("http://localhost", "https://test-revise.org").allowCredentials(true);
     }
 
 }
