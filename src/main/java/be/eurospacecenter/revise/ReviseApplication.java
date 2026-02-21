344e1f27fefa1a9169bbd54323577454e5f2597d
Hugo Raskin <raskin.hugo@gmail.com>
2026-02-21 09:34:13 +0100

    Fixing scheduling

--- src/main/java/be/eurospacecenter/revise/ReviseApplication.java
+++ src/main/java/be/eurospacecenter/revise/ReviseApplication.java
@@ -2,8 +2,10 @@ package be.eurospacecenter.revise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReviseApplication {
 
 	public static void main(String[] args) {SpringApplication.run(ReviseApplication.class, args);}
